f47c3878f330666a1ccc953a96c4c1ca1bea61a9
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-25 00:06:53 -0300

    ajusta mapeamento do ID

--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 @Entity
 public class Cliente {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String nome;
