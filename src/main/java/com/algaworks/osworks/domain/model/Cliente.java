9889b203d565e4df2fc4f261a48d28106206edce
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-24 23:34:39 -0300

    mapeia tabela com anotações do Java

--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,13 +1,20 @@
 package com.algaworks.osworks.domain.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
     private String nome;
